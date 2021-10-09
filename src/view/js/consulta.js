e2a1f3e4e845c2b907f81bde99427618f548c44c
IrmaRamirez <iriraga@hotmail.com>
2021-10-09 01:28:38 -0500

    Prescindir de los botones de consulta y editar del HTML. Implementación de la función de eliminación de filas (bugs)

--- src/view/js/consulta.js
+++ src/view/js/consulta.js
@@ -1,6 +1,8 @@
 // let database_1 = require("../../database/conexion");
 // let query_server = require("../../js/query_server.js");
 
+let cant_fila=0;
+
 // Instrucción que se ejecuta después de cargar la página.
 document.addEventListener("DOMContentLoaded", function (event) {
   // Revisar el localStorage
@@ -11,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   // Recorrer el arreglo y visualizar
   arrayJson.forEach((row) => {
-    show_newRowInTable(row, "tblSalidaDato");
+    show_newRowTable(row, "tblSalidaDato");
+    cant_fila++;
   });
 });
 
@@ -46,14 +49,19 @@ function addRow(event) {
   // Almacenar la información del formulario en una variable.
   const form_data = new FormData(form_user);
 
-  //Convertir la información del formulario en un JSon
+  // Convertir la información del formulario en un JSon
   const json_data = FormData2Obj(form_data);
+
+  cant_fila++;
+
+  // Añadimos el parámetro id a la información json_data de cada fila, según la cantidad de filas de la tabla en el momento del evento del botón "agregar".
+  json_data.idRow = cant_fila;// document.getElementById("tblSalidaDato").rows.length;
 
   // Guarda la información del formulario en el LocalStorage
   saveObjData("flujosDataLocalStorage", json_data);
 
   // Inserta la información en la tabla
-  show_newRowInTable(json_data, "tblSalidaDato");
+  show_newRowTable(json_data, "tblSalidaDato");
 
   // Reiniciar los valores de las casillas del formulario
   form_user.reset();
@@ -61,18 +69,19 @@ function addRow(event) {
   console.log("Curso Añadido");
 }
 
-function updRow(event) {
+// Función para eliminar una fila.
+function delRow(event,idRow) {
   // Evitar el reinicio de la página dado al activar eventos. Al presionar botón.
   event.preventDefault();
 
-  console.log("Curso Editado");
-}
-function delRow(event) {
-  // Evitar el reinicio de la página dado al activar eventos. Al presionar botón.
-  event.preventDefault();
+  // Capturar el elemento, que se asume es una tabla, para que pueda ser manipulado.
+  let tableBody = document.getElementById("tblSalidaDato");
 
-  console.log("Curso Eliminado");
+  // Ejecutar instrucción de eliminar la fila identificada
+  tableBody.deleteRow(idRow);
 }
+
+// Función para obtener los valores de toda la database.
 function getRow(event) {
   // Evitar el reinicio de la página dado al activar eventos. Al presionar botón.
   event.preventDefault();
@@ -101,34 +110,49 @@ function FormData2Obj(form_data) {
   return {
     areaFormUser: form_data.get("nameArea"),
     cursoFormUser: form_data.get("nameCurso"),
-    emailFormUser: "Ramiro",
+    emailFormUser: "Ramiro"
   };
 }
-
-function show_newRowInTable(diccForm, idTable) {
-  // console.log(diccForm);
+// Dibujar una fila en la tabla
+function show_newRowTable(diccForm, idTable) {
+  // Capturar el elemento, que se asume es una tabla, para que pueda ser manipulado.
   let tableBody = document.getElementById(idTable);
 
-  //
-  let ref_newRowTableBody = tableBody.insertRow(-1);
+  // Inserta una nueva fila a la tabla, ubicándola en la última posición, y guardará en una variable la referencia hacia esta fila.
+  let newRowTableBody = tableBody.insertRow(-1);
 
-  let newCell;
-  newCell = ref_newRowTableBody.insertCell(0);
-  newCell.textContent = diccForm["areaFormUser"];
+  // Creamos una variable que representa a la nueva fila.
+  let newRow;
 
-  newCell = ref_newRowTableBody.insertCell(1);
-  newCell.textContent = diccForm["cursoFormUser"];
+  // Creamos la primera celda de la fila creada.
+  newRow = newRowTableBody.insertCell(0);
+  // Escribimos el contenido de la celda creada.
+  newRow.textContent = diccForm["areaFormUser"];
 
-  newCell = ref_newRowTableBody.insertCell(2);
-  newCell.textContent = diccForm["emailFormUser"];
+  // Creamos la primera celda de la fila creada.
+  newRow = newRowTableBody.insertCell(1);
+  // Escribimos el contenido de la celda creada.
+  newRow.textContent = diccForm["cursoFormUser"];
 
-  newCell = ref_newRowTableBody.insertCell(3);
+  // Creamos la primera celda de la fila creada.
+  newRow = newRowTableBody.insertCell(2);
+  // Escribimos el contenido de la celda creada.
+  newRow.textContent = diccForm["emailFormUser"];
 
-  const div_newCell = document.createElement("div"),
-    strBtnEdit =
-      "<input type='submit' onclick='updRow(event);' value='Editar' />",
-    strBtnDel =
-      "<input type='submit' onclick='delRow(event)' value = 'Borrar' />";
-  div_newCell.innerHTML = strBtnEdit + strBtnDel;
-  newCell.appendChild(div_newCell);
+  // Creamos la primera celda de la fila creada.
+  newRow = newRowTableBody.insertCell(3);
+  // Escribimos el contenido de la celda creada y creamos una etiqueta 'div' dentro de esta celda, en el HTML.
+  newRow.textContent = document.createElement("div");
+
+  // console.log(diccForm['idRow']);
+  // Generamos los textos correspondientes a la creación de nuevas etiquetas html de tipo botones de envio de formulario (submit)
+  // const strBtnEdit =
+  //   "<input type='submit' onclick='updRow(event);' value='Editar' />";
+  const strBtnDel =
+    "<input type='submit' onclick='delRow(event," + diccForm['idRow'] + ")' value ='Borrar' />";
+
+  // Guardamos en el HTML el texto necesario correspondiente para la generación de los nuevos elementos que estarán dentro de la celda recien creada.
+  newRow.innerHTML =
+    //  strBtnEdit +
+    strBtnDel;
 }
